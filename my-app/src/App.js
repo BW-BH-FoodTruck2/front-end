38f8ca1df02ff67791ea9392cb5912c96336a62f
Kris Monroe <krisdmonroe@gmail.com>
2019-12-27 13:56:05 -0800

    HomePage Done Sign up Page in the making

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import {Route, Link, Switch} from "react-router-dom"
+
+import WelcomePage from "../src/components/WelcomePage"
+import SignUpPage from "../src/components/SignUpPage"
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+<Link to='/'> Home </Link> 
+<Link to='/SignUp'> SignUp </Link> 
+
+<Switch>
+       <Route exact path="/SignUp">
+       <SignUpPage/>
+       </Route>
+       
+       <Route exact path="/">
+       <WelcomePage/>
+       </Route>
+</Switch>
+</main>
   );
 }
 
---
+++ my-app/src/components/WelcomePage.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Form = styled.form`
+display:flex
+flex-direction:column
+background-color: grey
+width:30.5%
+height:310px
+align-items:center
+justify-content:center
+margin:0 26.5%
+margin-top:8.5%
+`;
+const Div = styled.div`
+  display:flex
+  flex-direction:column
+  margin:1%
+  justify-content:center
+`;
+const Button = styled.button`
+margin:0 auto
+`;
+const H1 = styled.h1`
+margin: 2% 26.5%
+
+`;
+const H2 = styled.h2`
+ margin 2% auto
+`;
+
+export default function WelcomePage(){
+    return (
+<section className="welcome-page">
+      
+        <H1>Welcome to the best place to find food trucks near you!</H1>
+        <Form> 
+      <Div>
+      <H2> Sign In </H2>
+      <input 
+      id='userName'
+      placeholder='UserName'
+      name='UserName'
+      type='text'
+      />
+    </Div>
+    <Div>
+    <input 
+      id='password'
+      placeholder='Password'
+      name='Password'
+      type='text'
+      />
+      <br/>
+      <Button> Sign Up </Button>
+      </Div>
+    </Form>
+      
+    </section>
+
+    );
+}
