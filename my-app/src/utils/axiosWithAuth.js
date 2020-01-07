7882475eec177adb40349928f025015ec6f0d8fe
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2020-01-07 13:28:20 -0700

    Login and signup working

---
+++ my-app/src/utils/axiosWithAuth.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const axiosWithAuth = () => {
+	const token = localStorage.getItem('token');
+
+	return axios.create({
+		baseURL : 'https://food-truck-trackr.herokuapp.com/api/auth',
+		headers : {
+			'Content-Type' : 'application/json',
+			authorization  : token
+		}
+	});
+};
